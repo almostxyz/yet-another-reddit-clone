8110edb215f85fbe1af4c8b0427a96efc2865cf0
Ruslan <almostz@yandex.ru>
2020-09-27 11:47:17 +0400

    :card_file_box: edit entities
    create new fields and many-to-one and one-to-many relations between User entity and Post entity

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,15 @@
-import { Resolver, Query, Arg, Int, Mutation } from "type-graphql";
+import { isAuth } from "../middlewares/isAuth";
+import { MyContext } from "src/types";
+import { Resolver, Query, Arg, Int, Mutation, InputType, Field, Ctx, UseMiddleware } from "type-graphql";
 import { Post } from "../entities/Post";
+
+@InputType()
+class PostInput {
+    @Field()
+    title: string
+    @Field()
+    text: string
+}
 
 @Resolver()
 export class PostResolver {
@@ -16,10 +26,15 @@ export class PostResolver {
     }
 
     @Mutation(() => Post)
+    @UseMiddleware(isAuth)
     async createPost(
-        @Arg('title') title: string,
+        @Arg('input') input: PostInput,
+        @Ctx() { req }: MyContext
     ): Promise<Post> {
-        return Post.create({ title }).save()
+        return Post.create({
+            ...input,
+            creatorId: req.session.userId
+        }).save()
     }
 
     @Mutation(() => Post, { nullable: true })
